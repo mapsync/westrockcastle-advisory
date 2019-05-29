cc45563b79d5cbc69fb9a0975d843fdd7832d731
unknown <caseyy@mapsync.com>
2019-05-29 19:52:15 -0400

    nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -22,6 +22,7 @@ const Navbar = class extends React.Component {
             <Link to="/" className="btn btn-link" title="Logo">
               <img src="/img/small-logo.png" alt="" />
             </Link>
+            <div>&nbsp;</div>
             <Link className="btn pt-2" activeClassName="active" to="/contact">
               <Icon.Phone size={26}/>
               <div>Contact</div>
@@ -46,6 +47,7 @@ const Navbar = class extends React.Component {
             </Link>
           </section>
           <section className="navbar-section">
+            <div>&nbsp;</div>
             <Link className="btn btn-link" activeClassName="active" to="/contact">
               Contact
             </Link>
