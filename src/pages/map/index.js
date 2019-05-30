6967fb125f7868c38b64058d0cdfe05e9ea78b12
unknown <caseyy@mapsync.com>
2019-05-30 11:10:39 -0400

    map fix

--- src/pages/map/index.js
+++ src/pages/map/index.js
@@ -3,7 +3,7 @@ import Layout from '../../components/Layout'
 
 export default class Index extends React.Component {
   componentDidMount() {
-    document.getElementById("map").src = "https://geosync.cloud/maps/c7028e29-93b1-4897-8de9-73cb6dff8d14" + this.props.location.search;
+    document.getElementById("map").src = "https://geosync.cloud/maps/e3c33059-60a3-4d5b-a5bc-7601370fc604" + this.props.location.search;
   }
 
   render() {
